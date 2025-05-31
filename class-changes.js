bd9b10c28a2530671331dfbea269444ee7e5a67d
Francis.tan <kidperson057@gmail.com>
2025-05-31 10:02:34 +1000

    edit

---
+++ class-changes.js
@@ -0,0 +1,79 @@
+const classChanges = [
+
+  /* side images */
+
+  { time: 0, target: ".slide1", add: "show" },
+  { time: 10, target: ".slide1", remove: "show" },
+  { time: 10, target: ".slide2", add: "show" },
+  { time: 11, target: ".slide2", remove: "show" },
+  { time: 11, target: ".slide3", add: "show" },
+  { time: 27, target: ".slide3", remove: "show" },
+  { time: 27, target: ".slide4", add: "show" },
+  { time: 31, target: ".slide4", remove: "show" },
+  { time: 31, target: ".slide5", add: "show" },
+  { time: 32, target: ".slide5", remove: "show" },
+  { time: 32, target: ".slide6", add: "show" },
+  { time: 35, target: ".slide6", remove: "show" },
+  { time: 35, target: ".slide7", add: "show" },
+  { time: 38, target: ".slide7", remove: "show" },
+  { time: 38, target: ".slide8", add: "show" },
+  { time: 39, target: ".slide8", remove: "show" },
+  { time: 39, target: ".slide9", add: "show" },
+  { time: 44, target: ".slide9", remove: "show" },
+  { time: 44, target: ".slide10", add: "show" },
+  { time: 48, target: ".slide10", remove: "show" },
+  { time: 48, target: ".slide11", add: "show" },
+  { time: 52, target: ".slide11", remove: "show" },
+  { time: 52, target: ".slide12", add: "show" },
+  { time: 73, target: ".slide12", remove: "show" },
+  { time: 73, target: ".slide10", add: "show" },
+  { time: 94, target: ".slide10", remove: "show" },
+  { time: 94, target: ".slide14", add: "show" },
+  { time: 102, target: ".slide14", remove: "show" },
+  { time: 102, target: ".slide15", add: "show" },
+  { time: 112, target: ".slide15", remove: "show" },
+  { time: 112, target: ".slide10", add: "show" },
+  { time: 125, target: ".slide10", remove: "show" },
+  { time: 125, target: ".slide17", add: "show" },
+  { time: 138, target: ".slide17", remove: "show" },
+  { time: 138, target: ".slide10", add: "show" },
+  { time: 175, target: ".slide10", remove: "show" },
+  { time: 175, target: ".slide19", add: "show" },
+  { time: 183, target: ".slide19", remove: "show" },
+  { time: 183, target: ".slide10", add: "show" },
+  { time: 213, target: ".slide10", remove: "show" },
+  { time: 213, target: ".slide14", add: "show" },
+  { time: 240, target: ".slide14", remove: "show" },
+  { time: 240, target: ".slide22", add: "show" },
+  { time: 242, target: ".slide22", remove: "show" },
+  { time: 242, target: ".slide23", add: "show" },
+  { time: 249, target: ".slide23", remove: "show" },
+  { time: 249, target: ".slide24", add: "show" },
+  { time: 276, target: ".slide24", remove: "show" },
+  { time: 276, target: ".slide25", add: "show" },
+
+  /* highlight text */
+
+  { time: 6, target: ".t2", add: "highlight" },
+  { time: 13, target: ".t2", remove: "highlight" },  
+
+  /* invert */
+
+  { time: 102, target: ".main", add: "invert" },
+  { time: 103, target: ".main", remove: "invert" },
+  { time: 175, target: ".main", add: "invert" },
+  { time: 175, target: ".main", remove: "invert" },
+
+  { time: 102, target: ".sidebar", add: "invert" },
+  { time: 103, target: ".sidebar", remove: "invert" },  
+  { time: 175, target: ".sidebar", add: "invert" },
+  { time: 175, target: ".sidebar", remove: "invert" },  
+
+  /* hide */
+
+  { time: 27, target: ".main", add: "hide" },
+  { time: 28, target: ".main", remove: "hide" },
+
+  { time: 27, target: ".sidebar", add: "hide" },
+  { time: 28, target: ".sidebar", remove: "hide" },
+];
